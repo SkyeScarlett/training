573efb703d49369568cf61b726cac35e1eb89211
Skye <anastasia_sorokina_90@mail.ru>
2023-03-27 14:35:47 +0300

    familytree ready

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,17 @@
 class Person < ApplicationRecord
 
-	belongs_to :mother, class_name: Person
-	belongs_to :father, class_name: Person
+	belongs_to :mother, class_name: 'Person', optional: true
+	belongs_to :father, class_name: 'Person', optional: true
 
 	validates :gender, inclusion: { in: ['male', 'female']}, presence: true
 	validates :name, presence: true
 
+
+	def children
+		if gender == 'male'
+			Person.where(father_id: id)
+		else
+			Person.where(mother_id: id)
+		end
+	end
 end
